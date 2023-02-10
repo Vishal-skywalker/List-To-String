a3c83d6947319006e3b00dd038e4a0304885ddaa
dasv <vishal.das@mkcorp.com>
2023-02-10 19:34:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+function convert(){
+    const sperator = document.getElementById('sperator');
+    const wholeEnclosure = document.getElementById('wholeEnclosure');
+    const eachEnclosure = document.getElementById('eachEnclosure');
+    const input = document.getElementById('input');
+    const output = document.getElementById('output');
+    let type = '';
+    document.getElementsByName('type').forEach(el => {
+        if (el.checked) {
+            type = el.value;
+        }
+    });
+    const regex = new RegExp(/\s/, 'ig');
+    let data = input.value;
+    data = data.split(regex).filter(function(el) {
+        if(!el.match(regex)){
+            return el.trim();
+        } 
+    });
+    if(type === 'set'){
+        data = [... new Set(data)];
+    }
+    data = enclose(eachEnclosure.value, true, data);
+    data = enclose(wholeEnclosure.value, false, data.join(sperator.value));
+    output.value = data;
+}
+
+function enclose(enclosure, isEach, data) {
+    switch(enclosure) {
+        case 'single':
+            if (isEach) {
+                return each(`'****'`, data);
+            }
+            return whole(`'****'`, data);
+        case 'double':
+            if (isEach) {
+                return each(`"****"`, data);
+            }
+            return whole(`"****"`, data);
+        case '()':
+            if (isEach) {
+                return each(`(****)`, data);
+            }
+            return whole(`(****)`, data);
+        case '{}':
+            if (isEach) {
+                return each(`{****}`, data);
+            }
+            return whole(`{****}`, data);
+        case '[]':
+            if (isEach) {
+                return each(`[****]`, data);
+            }
+            return whole(`[****]`, data);
+        case 'none':
+        default : 
+            return data;
+    }
+}
+
+function each(enclosure, data) {
+    const returnData = [];
+     data.forEach(element => {
+        returnData.push( enclosure.replace('****', element));
+    });
+    return returnData;
+}
+function whole(enclosure, data) {
+    return enclosure.replace('****', data);
+}
